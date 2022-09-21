9ce51b3a5ef12f60c2571ace1d7d8fe44925e74e
Jules Huisman <huisman.jules@gmail.com>
2022-09-21 16:33:22 +0000

    Removed dagit requirement

--- setup.py
+++ setup.py
@@ -22,7 +22,6 @@ setuptools.setup(
     ],
     install_requires=[
         "dagster~=1.0.7",
-        "dagit~=1.0.7",
         "dagster-pandas~=0.16.7",
         "pandas~=1.4.4",
         "requests~=2.28.1",
